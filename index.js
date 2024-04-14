9e6167cf5b74fd5802bd41a6950e7ff880b7092d
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 21:23:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// Q66
+//make a function check two boolean value using && operator
+function check(a, b) {
+    return a && b;
+}
+console.log(check(true, false));
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q66
+//make a function check two boolean value using && operator
+function check(a:boolean,b:boolean){
+    return a && b;
+}
+console.log(check(true, false));// it check two value if one is false answer is false
